2e6a2a3a018c0614ae36d797f48d76b291dafb32
JcdeA <io@fosshost.org>
2022-03-31 22:39:24 +0000

    fix: add secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from 'lib/prisma'
 
 export default NextAuth({
+  secret: process.env.SECRET,
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
