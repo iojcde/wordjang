e2c6701c6fcba41352b08491825d53a88e03a411
JcdeA <io@fosshost.org>
2022-04-01 16:28:20 +0000

    fix: use env vars for db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,13 @@ import { PrismaClient } from '@prisma/client'
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === `production`) {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: `${process.env.DATABASE_URL}&sslidentity=${process.env.DB_CA}`,
+      },
+    },
+  })
 } else {
   const globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient
