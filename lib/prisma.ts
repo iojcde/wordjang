e9b9dab810791e99e5e3e1f846bf426b41b66b4a
JcdeA <io@fosshost.org>
2022-04-01 16:41:45 +0000

    feat: use vercel.json includeFiles

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,13 +3,7 @@ import { PrismaClient } from '@prisma/client'
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === `production`) {
-  prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: `${process.env.DATABASE_URL}&sslidentity=${process.env.DB_CA}`,
-      },
-    },
-  })
+  prisma = new PrismaClient()
 } else {
   const globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient
